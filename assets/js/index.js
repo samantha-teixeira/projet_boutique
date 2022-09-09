ca567a9b5046b7a9cf689a5a1dcfe59eea11ba63
samantha-teixeira <samerika69@gmail.com>
2022-09-09 23:47:30 +0200

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,15 @@
-let burger = document.getElementById("burger");
+let btn_burger = document.getElementById("btn_burger");
 let menu = document.getElementById("menu");
 let menu_container = document.getElementById("menu_container");
 let menu2 = document.getElementById("menu2");
 let close = document.getElementById("close");
+let btn_close = document.getElementById("btn_close");
 
-burger.addEventListener("click",ouvrirBurger);
+btn_burger.addEventListener("click",ouvrirMenu);
 
-function ouvrirBurger() {
+function ouvrirMenu() {
     if(menu_container.style.display="none") {
+        menu_container.style.display="block"
         menu2.style.display="flex";
         menu2.style.display="block";
         burger.style.display = "none";
@@ -18,8 +20,8 @@ function ouvrirBurger() {
     }
 };
 
-close.addEventListener('click', fermerMenu);
-console.log(close);
+btn_close.addEventListener('click', fermerMenu);
+menu_container.addEventListener('click',fermerMenu);
 
 function fermerMenu() {
     menu2.style.display= 'none';
@@ -28,16 +30,12 @@ function fermerMenu() {
 
     if (menu_container.style.display = 'block') {
         menu_container.style.display = 'none';
-        
-
     } else {
         return false;
     }
-    
 };
 
-
-function myFunction1(largeScreen) {
+function Function1(largeScreen) {
     if (largeScreen.matches) { // If media query matches
         menu_container.style.display = 'none';
         menu2.style.display="none";
@@ -48,8 +46,8 @@ function myFunction1(largeScreen) {
         burger.style.display = "flex";
         menu.style.display = "none";
     }
-}
+};
 
 let largeScreen = window.matchMedia("(min-width: 701px)")
-myFunction1(largeScreen) // Call listener function at run time
-largeScreen.addListener(myFunction1)
+Function1(largeScreen) // Call listener function at run time
+largeScreen.addListener(Function1)
